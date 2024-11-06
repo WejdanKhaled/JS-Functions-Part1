5d89ca6bd784ef22d74dfbff69e8a44e4a9d9c1b
Wejdan Khaled <vauseq8@icloud.com>
2024-11-06 17:55:26 +0300

    done

---
+++ functions1.js
@@ -0,0 +1,34 @@
+function printName()
+{ console.log ("Wejdan"); }
+
+ printName();
+
+function printAge(birthYear)
+{ console.log (2024-birthYear);}
+
+ printAge(2000);
+
+function printAgeAndName (birthYear, name)
+{console.log ( `Hello ${name} you arre ${2024 - birthYear} years old`);}
+
+printAgeAndName(2000,"Wejdan");
+
+function printHello(name, language)
+{ if (language ==="en")
+     { console.log(`Hello ${Name}`);}
+     
+    else if (language ==="es")
+     {console.log(`Hola ${Name}`);}
+
+    
+}
+printHello("Wejdan", "en")
+function printMax (firstNumber, secondNumber){
+    if (firstNumber > secondNumber)
+    {console.log(firstNumber);}
+    
+    else {console.log(secondNumber);}
+
+  
+}
+  printMax(13, 5);
